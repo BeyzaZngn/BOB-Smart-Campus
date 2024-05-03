d923f447af2dc7295573375c862d0b35ce6baf3a
BeyzaZngn <beyzazengin76@gmail.com>
2024-05-03 18:25:55 +0300

    BOB created

---
+++ BOB/BOB/BOBApp.swift
@@ -0,0 +1,17 @@
+//
+//  BOBApp.swift
+//  BOB
+//
+//  Created by Beyza Zengin on 3.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct BOBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
