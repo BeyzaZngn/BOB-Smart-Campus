73f404d8992556f055a0dfa1bcf314361eb26a52
BeyzaZngn <beyzazengin76@gmail.com>
2024-05-03 23:46:23 +0300

    completed

--- BOB_Kulupler/BOB_Kulupler/ContentView.swift
+++ BOB_Kulupler/BOB_Kulupler/ContentView.swift
@@ -4,7 +4,6 @@
 //
 //  Created by Beyza Zengin on 3.05.2024.
 //
-
 import SwiftUI
 
 struct ContentView: View {
@@ -24,16 +23,16 @@ struct ContentView: View {
 
 struct SecondView: View {
     let columns = Array(repeating: GridItem(.flexible(), spacing: 15), count: 3)
+    let buttonNames = ["Careify", "Artun İHA", "URAZ"]
     
     var body: some View {
+       
         ScrollView {
-            
             VStack {
                 LazyVGrid(columns: columns, spacing: 15) {
-                    ForEach(1...3, id: \.self) { index in
-                        NavigationLink(destination: DetailViewFirst(buttonIndex: index)) {
-                            Text("Buton \(index)")
-                                .padding()
+                    ForEach(buttonNames.indices, id: \.self) { index in
+                        NavigationLink(destination: destinationView(for: index)) {
+                            Text("\(buttonNames[index])")                                .padding()
                                 .frame(maxWidth: .infinity)
                                 .background(Color.yellow)
                                 .foregroundColor(.white)
@@ -46,20 +45,158 @@ struct SecondView: View {
             }
         }
     }
+    
+    func destinationView(for index: Int) -> some View {
+        switch index {
+        case 0:
+            return AnyView(DetailViewFirst())
+        case 1:
+            return AnyView(DetailViewSecond())
+        case 2:
+            return AnyView(DetailViewThird())
+        default:
+            return AnyView(EmptyView())
+        }
+    }
 }
 
+
 struct DetailViewFirst: View {
-    let buttonIndex: Int
-    
     var body: some View {
         VStack {
-            Form{
-                Section ("Careify") {
+            Form {
+                Section {
+                    Text("Takımımız 2021 yılında, TEKNOFEST Uluslararası İHA yarışmasının ilk rapor gönderimine 14 gün kala 14 Şubat'ta kurulmuştur. Bilgisayar ve Elektronik mühendisliği bölümlerden bir grup öğrenci bir araya gelip rapor yazıp başvuru yapmıştır. Bilgisayar ve elektronik kısımlarından tam puan alınmış, mekanik eksikliğinden dolayı başarıya ulaşılamamıştır.")
+                } header: {
+                    Text("Kimdir?")
+                        .textCase(nil)
+                }
+                Section {
                     
-                    Text("Buton \(buttonIndex) Tıklandı!")
+                    HStack {
+                        Button(action: {
+                            if let url = URL(string: "https://www.careify.app") {
+                                UIApplication.shared.open(url)
+                            }
+                        }, label: {
+                            Image("png-clipart-computer-icons-website-icon-text-globe-thumbnail")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(10)
+                        })
+                        Text("Web Sitesi")
+                    }
+                    
+                } header: {
+                    Text("Sosyal Medya")
+                        .textCase(nil)
                 }
             }
         }
+        .navigationTitle("Careify Takımı")
+    }
+}
+
+struct DetailViewSecond: View {
+    var body: some View {
+        VStack {
+            Form {
+                Section {
+                    Text("Takımımız 2021 yılında, TEKNOFEST Uluslararası İHA yarışmasının ilk rapor gönderimine 14 gün kala 14 Şubat'ta kurulmuştur. Bilgisayar ve Elektronik mühendisliği bölümlerden bir grup öğrenci bir araya gelip rapor yazıp başvuru yapmıştır. Bilgisayar ve elektronik kısımlarından tam puan alınmış, mekanik eksikliğinden dolayı başarıya ulaşılamamıştır.")
+                } header: {
+                    Text("Kimdir?")
+                        .textCase(nil)
+                }
+                Section {
+                    HStack {
+                        Button(action: {
+                            if let url = URL(string: "https://www.linkedin.com/company/artuniha/") {
+                                UIApplication.shared.open(url)
+                            }
+                        }, label: {
+                            Image("LinkedIn_logo_initials")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(10)
+                        })
+                        Text("LinkedIn")
+                    }
+                    
+                    HStack {
+                        Button(action: {
+                            if let url = URL(string: "https://www.instagram.com/artuniha?igsh=MWlkZWsyaDlvazVp") {
+                                UIApplication.shared.open(url)
+                            }
+                        }, label: {
+                            Image("Instagram_icon")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(10)
+                        })
+                        Text("Instagram")
+                    }
+                    
+                } header: {
+                    Text("Sosyal Medya")
+                        .textCase(nil)
+                }
+            }
+        }
+        .navigationTitle("Artun İHA Takımı")
+    }
+}
+
+struct DetailViewThird: View {
+    var body: some View {
+        VStack {
+            Form {
+                Section {
+                    Text("URAZ Model Uydu Takımı 2021 yılında Kütahya Dumlupınar Üniversitesi, Uzay ve Havacılık Topluluğu bünyesinde havacılık bilimi ve endüstrisine ilgili öğrenciler tarafından kurulmuştur. Aynı zamanda TEKNOFEST Koordinartörlüğü bünyesinde yer almaktadır.Bu öğrenciler; Bilim ve Teknolojik gelişmelere teşvik etmek, bilgi ve tecrübelerini katlayarak paylaşmak amacıyla 2021-2022 yılından itibaren Teknofest 7.Model Uydu ve 8.Model Uydu Yarışmalarına katılım sağlamıştır.")
+                } header: {
+                    Text("Kimdir?")
+                        .textCase(nil)
+                }
+                Section {
+                    HStack {
+                        Button(action: {
+                            if let url = URL(string: "https://www.linkedin.com/company/urazmut/?originalSubdomain=tr") {
+                                UIApplication.shared.open(url)
+                            }
+                        }, label: {
+                            Image("LinkedIn_logo_initials")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(10)
+                        })
+                        Text("LinkedIn")
+                    }
+                    
+                    HStack {
+                        Button(action: {
+                            if let url = URL(string: "https://www.instagram.com/urazmut?igsh=MWwxa3hqaHNiZ3FzMw==") {
+                                UIApplication.shared.open(url)
+                            }
+                        }, label: {
+                            Image("Instagram_icon")
+                                .resizable()
+                                .scaledToFit()
+                                .frame(width: 50, height: 50)
+                                .cornerRadius(10)
+                        })
+                        Text("Instagram")
+                    }
+                    
+                } header: {
+                    Text("Sosyal Medya")
+                        .textCase(nil)
+                }
+            }
+        }
+        .navigationTitle("Uraz Model Uydu Takımı")
     }
 }
 
@@ -67,49 +204,4 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
-    
-    
-    struct DetailViewSecond: View {
-        let buttonIndex: Int
-        
-        var body: some View {
-            VStack {
-                Form{
-                    Section ("Careify") {
-                        
-                        Text("Buton \(buttonIndex) Tıklandı!")
-                    }
-                }
-            }
-        }
-    }
-    
-    struct ContentView_Previews: PreviewProvider {
-        static var previews: some View {
-            ContentView()
-        }
-        
-        struct DetailViewThird: View {
-            let buttonIndex: Int
-            
-            var body: some View {
-                VStack {
-                    Form{
-                        Section ("Careify") {
-                            
-                            Text("Buton \(buttonIndex) Tıklandı!")
-                        }
-                    }
-                }
-            }
-        }
-        
-        struct ContentView_Previews: PreviewProvider {
-            static var previews: some View {
-                ContentView()
-            }
-            
-        }
-    }
-    
 }
