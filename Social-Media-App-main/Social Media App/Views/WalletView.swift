085e0ccc890d69ba14582d60036621608a61af7a
Ali Osman Öztürk <developer@aliosmanozturk.me>
2024-05-05 11:39:52 +0300

    Update WalletView.swift

--- Social-Media-App-main/Social Media App/Views/WalletView.swift
+++ Social-Media-App-main/Social Media App/Views/WalletView.swift
@@ -1,15 +1,47 @@
-//
-//  WalletView.swift
-//  Social Media App
-//
-//  Created by Ali Osman Öztürk on 5.05.2024.
-//
-
 import SwiftUI
 
 struct WalletView: View {
+    var balance: Double  = 255.90 // Sayaçtan alınan değeri tutacak değişken
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        VStack {
+            Spacer()
+            Text("5234 5678 9345 3421")
+                .font(.title)
+                .foregroundColor(.white)
+                .padding()
+            
+            Spacer()
+            
+            VStack {
+                HStack {
+                    Text("Bakiye: \(balance.formatted())") // Sayaç değerini burada göstereceğiz
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .padding()
+                }
+            }
+            
+            Spacer()
+            
+            HStack {
+                Text("01/25")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+                Spacer()
+                Text("CVV: 123")
+                    .font(.headline)
+                    .foregroundColor(.white)
+                    .padding()
+            }
+            .padding(.horizontal)
+        }
+        .frame(width: 350, height: 200)
+        .background(Color.blue)
+        .cornerRadius(20)
+        .shadow(radius: 5)
+        .padding()
     }
 }
 
